523cd4c8da4e12dba68db01ddd5c24577d22666f
Jon <jonathanjackson424@gmail.com>
2019-11-01 15:21:11 -0400

    final version

--- dog-app.js
+++ dog-app.js
@@ -8,6 +8,15 @@ function fetchAllDogImages(countNumber){
       displayResults(responseJson))
     .catch(error => alert(error.message));
 }
+//generate random image by breed
+function randomByBreed(breed){
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      breedResults(responseJson))
+    .catch(error => alert(error.message));
+}
+
 function displayResults(responseJson) {
   console.log(responseJson);
   const imageElements = [];
@@ -20,15 +29,33 @@ function displayResults(responseJson) {
   console.log(imageElements);
 }
 
+function breedResults(responseJson) {
+
+
+  let newString = `<img src ="${responseJson.message}" class ="results-img">`;
+  $('.results').html(newString).removeClass('hidden');
+}
+
+
 function watchForm() {
-  $('form').submit(event => {
+  $('#submit').submit(event => {
     event.preventDefault();
     let countValue = event.target.imgcount.value;
-    fetchAllDogImages(countValue);
+    if (countValue <=50){
+      fetchAllDogImages(countValue);
+    }else {
+      alert('Please insert a number between 1 and 50');
+    }
+  });
+  $('#breed').submit(event => {
+    event.preventDefault();
+    let breed = event.target.breedimg.value;
+    randomByBreed(breed);
   });
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  breedResults();
 });
